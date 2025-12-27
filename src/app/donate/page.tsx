7cbdbff06b4514040721f98f3c84e7753d662976
ritikaaramani <ritika.aramani06@gmail.com>
2025-12-27 21:40:38 +0530

    build issues

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -19,7 +19,6 @@ export default function DonatePage() {
       router.push('/login');
     }
   }, [isUserLoggedIn, router]);
-  }, [router]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
